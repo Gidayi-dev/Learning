d9b21979312cc68a509e904c1ee26613a34947f6
Gidayi-dev <millyannahi@gmail.com>
2025-11-28 17:28:44 +0300

    Numbers in C#

--- C#/Introduction/Introduction/Program.cs
+++ C#/Introduction/Introduction/Program.cs
@@ -55,6 +55,42 @@ namespace Introduction
             Console.WriteLine(lyrics.Contains("baby"));
             Console.WriteLine(lyrics.StartsWith("Baby"));
             Console.WriteLine(lyrics.EndsWith("drive"));
+
+            // Numbers
+            // integers
+            int a = 34;
+            int b = 90;
+            int d = 105;
+            // operations
+            int c = b - a;
+            Console.WriteLine(c);
+            int e = d + a / (c) % 2;
+            Console.WriteLine(e);
+            int Max = int.MaxValue;
+            int Min = int.MinValue;
+
+            Console.WriteLine($"The Maximum value is {Max} and the Minimum value is {Min}");
+
+            // Double
+            double num1 = 5;
+            double num2 = 4;
+            double num3 = 2;
+            double num4 = (num1 + num2) / num3;
+            Console.WriteLine(num4);
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            double num5 = 1.0;
+            double num6 = 3.0;
+            Console.WriteLine(num5 / num6);
+
+            decimal num7 = 1.0M;
+            decimal num8 = 3.0M;
+            Console.WriteLine(num7 / num8);
         }
     }
 }
